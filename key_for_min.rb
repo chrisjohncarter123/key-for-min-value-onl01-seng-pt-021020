be9fc369ba19bc35b7a2d13dc0e774b467a4fe18
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-17 03:20:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,17 @@ def key_for_min_value(name_hash)
     NIL
   end
   
-  name_hash.each do ||
-    
+  min_key = ""
+  min = 0
+  first = true
+  name_hash.each do |key, value|
+    if(first)
+      min_key = key
+      min = value
+    elsif (value < min)
+      min_key = key
+      min = value
+    end
   end
+  min_key
 end
